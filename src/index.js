bcbffd530e11b939162c822179482683ddc6a014
matt8poi <matt8poi@gmail.com>
2020-05-15 16:07:37 -0600

    Starting part 2.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Stat = ({value, text}) => {
+  if (text === 'positive') {
+    return (
+      <tbody>
+        <tr>
+          <td>{text}</td>
+          <td>{value} %</td>
+        </tr>
+        </tbody>
+    )
+  }
+  return (
+    <tbody>
+      <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+        </tbody>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = ((good * 1) + (neutral * 0) + (bad * -1)) / all
+  const positive = good / all * 100
+
+  if (all === 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button onClick={() => setGood(good + 1)} text='good' />
+        <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button onClick={() => setBad(bad + 1)} text='bad' />
+        <h1>statistics</h1>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text='good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='bad' />
+      <h1>statistics</h1>
+      <table>
+        <Stat value={good} text='good' />
+        <Stat value={neutral} text='neutral' />
+        <Stat value={bad} text='bad' />
+        <Stat value={all} text='all' />
+        <Stat value={average} text='average' />
+        <Stat value={positive} text='positive' />
+      </table>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  )
